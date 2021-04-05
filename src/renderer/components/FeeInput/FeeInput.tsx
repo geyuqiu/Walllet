585306d9a0bcae1ade4fd14c6f3c34f79061cc2b
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-05 18:49:26 +0200

    * add handleTextBoxChange

--- src/renderer/components/FeeInput/FeeInput.tsx
+++ src/renderer/components/FeeInput/FeeInput.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 
-export const FeeInput = () =>
-	// {onChange}: Partial<FeeInputProps>
-	{
-		const [fee, feeChange] = useState("");
-		const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-			feeChange(event.target.value);
+export const FeeInput = ({ onChange }: Partial<FeeInputProps>) => {
+	const [fee, feeChange] = useState("");
+	const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+		feeChange(event.target.value);
+	const handleTextBoxChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+		feeChange(event.target.value);
 
-		return (
-			<>
-				<label htmlFor="fee">Fee:</label>
-				<input role="textbox" value={fee} />
-				<input
-					type="range"
-					id="fee"
-					name="fee"
-					min="0"
-					step="0.00000001"
-					role="slider"
-					onChange={handleChange}
-				/>
-			</>
-		);
-	};
+	return (
+		<>
+			<label htmlFor="fee">Fee:</label>
+			<input
+				role="textbox"
+				value={fee}
+				placeholder="Simple Text"
+				onChange={handleTextBoxChange}
+			/>
+			<input
+				type="range"
+				id="fee"
+				name="fee"
+				min="0"
+				step="0.00000001"
+				role="slider"
+				onChange={handleSliderChange}
+			/>
+		</>
+	);
+};
 
 type FeeInputProps = {
 	onChange: any;
