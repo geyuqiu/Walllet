b650294c5260e68ecb587a5937c617022ebbd573
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-05 21:04:09 +0200

    * add Footer

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 import { Routing } from "../router/Routing";
+import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 
 const notProd = process.env.NODE_ENV !== "production";
 
 export const App = () => {
-	const showDebugScreensOnDev: string = notProd ? "debug-screens" : "";
+	const showDebugScreensOnDev: string = notProd
+		? "debug-screens flex flex-col min-h-screen"
+		: "";
 
 	return (
 		<main className={showDebugScreensOnDev} role="main">
 			<Header />
+			<Footer />
 			<Routing />
 		</main>
 	);
---
+++ src/renderer/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => (
+	<footer className="bg-gray-100 p-6">
+		<div className="flex items-center">
+			<span>2020 &copy; ARK.io | All rights reserved</span>
+		</div>
+	</footer>
+);
