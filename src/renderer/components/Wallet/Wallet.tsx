2699272d54252ff0b95bf67845a6fb729802c8dc
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-06 21:19:54 +0200

    * add wallets as states

--- src/renderer/components/Wallet/Wallet.tsx
+++ src/renderer/components/Wallet/Wallet.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
-import { httpClient } from "../../services/HttpClient";
+import {httpClient} from "../../services/HttpClient";
 
 export const Wallet = () => {
+	const [wallets, setWallets] = useState([]);
 	useEffect(() => {
 		const fetchWallets = async () => {
-			const wallets = await httpClient.get(
+			setWallets(await httpClient.get(
 				"https://dwallets.ark.io/api/wallets?page=1&limit=5"
-			);
-			// console.info(wallets);
+			));
 		};
 		fetchWallets();
 	}, []);
 
-	return <div>Wallet</div>;
+	return <div>{JSON.stringify(wallets)}</div>;
 };
