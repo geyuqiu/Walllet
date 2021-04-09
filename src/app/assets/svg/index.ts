7fcc9bfa0a1937e03e6041a34a61453f8308ccf3
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-09 18:20:52 +0200

    * import svg

--- src/app/assets/svg/index.ts
+++ src/app/assets/svg/index.ts
@@ -1,7 +1,17 @@
+import {ReactComponent as Address} from './address.svg';
+import {ReactComponent as ArrowDown} from './arrow-down.svg';
+import {ReactComponent as ArrowUp} from './arrow-up.svg';
+import {ReactComponent as Balance} from './balance.svg';
 import {ReactComponent as ChevronDown} from "./chevron-down.svg";
+import {ReactComponent as Logo} from './logo.svg';
 import {ReactComponent as Txid} from './txid.svg';
 
 export const SvgCollection: any = {
 	ChevronDown,
 	Txid,
+	Address,
+	ArrowDown,
+	ArrowUp,
+	Balance,
+	Logo,
 };
