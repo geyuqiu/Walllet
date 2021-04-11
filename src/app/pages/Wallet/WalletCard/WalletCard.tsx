08a6f0cf75403d71dce056bf6ab268a73cf102ea
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-11 22:03:14 +0200

    clean up

--- src/app/pages/Wallet/WalletCard/WalletCard.tsx
+++ src/app/pages/Wallet/WalletCard/WalletCard.tsx
@@ -15,25 +15,23 @@ type WalletProps = {
 
 export const WalletCard = ({wallets, wallet, addressOnSelect}: WalletProps) => {
 	return (
-		<>
-			<Card className="bg-green-dark sm:flex sm:justify-center py-12 px-8">
-				<Card className="bg-black-darkest p-8 justify-between text-white rounded-lg w-full max-w-screen-xl flex justify-center lg:items-center flex-col lg:flex-row">
-					<LogoTitle/>
-					<div className="lg:hidden">
-						<Divider className="border-black-dark dark:border-theme-secondary-600" type="horizontal"/>
-					</div>
-					<div className="lg:contents hidden">
-						<Divider className="border-black-dark dark:border-theme-secondary-600" type="vertical"/>
-					</div>
-					<AddressDropdown
-						wallets={wallets} wallet={wallet} addressOnSelect={addressOnSelect}
-					/>
-					<div className="hidden lg:contents">
-						<Divider className="border-black-dark dark:border-theme-secondary-600" type="vertical"/>
-					</div>
-					<BalanceDisplay wallet={wallet}/>
-				</Card>
+		<Card className="bg-green-dark sm:flex sm:justify-center py-12 px-8">
+			<Card className="bg-black-darkest p-8 justify-between text-white rounded-lg w-full max-w-screen-xl flex justify-center lg:items-center flex-col lg:flex-row">
+				<LogoTitle/>
+				<div className="lg:hidden">
+					<Divider className="border-black-dark dark:border-theme-secondary-600" type="horizontal"/>
+				</div>
+				<div className="lg:contents hidden">
+					<Divider className="border-black-dark dark:border-theme-secondary-600" type="vertical"/>
+				</div>
+				<AddressDropdown
+					wallets={wallets} wallet={wallet} addressOnSelect={addressOnSelect}
+				/>
+				<div className="hidden lg:contents">
+					<Divider className="border-black-dark dark:border-theme-secondary-600" type="vertical"/>
+				</div>
+				<BalanceDisplay wallet={wallet}/>
 			</Card>
-		</>
+		</Card>
 	);
 };
