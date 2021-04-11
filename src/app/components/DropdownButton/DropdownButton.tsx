fb1714aa710e370745211d4b7360f33ffde41f0c
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-11 14:33:04 +0200

    * change icon on toggle

--- src/app/components/DropdownButton/DropdownButton.tsx
+++ src/app/components/DropdownButton/DropdownButton.tsx
@@ -6,31 +6,30 @@ import {Circle} from '../Circle/Circle';
 
 type DropdownButtonProps = {
 	isOpen: boolean;
-	label: React.ReactNode
+	label: React.ReactNode;
 }
 
-export const DropdownButton = ({isOpen, label}: DropdownButtonProps) => {
-	return (
-		<button className="flex justify-between items-center h-11 w-full focus:outline-none">
-			<div className="flex items-center">
-				<div className="hidden sm:block">
-					<Circle className="rounded-l-3xl border-r bg-black-light" size="lg">
-						<Icon name="Address" width={15} height={15} fill='#000000' stroke='#FBC457' className="ml-2"
-						      data-testid="dropdown__left__icon"/>
-					</Circle>
-				</div>
-				{label}
+export const DropdownButton = ({isOpen, label}: DropdownButtonProps) => (
+	<button className="flex justify-between items-center h-11 w-full focus:outline-none">
+		<div className="flex items-center">
+			<div className="hidden sm:block">
+				<Circle className="rounded-l-3xl border-r bg-black-light" size="lg">
+					<Icon name="Address" width={15} height={15}
+					      fill={isOpen ? '#029383' : '#000000'} stroke={isOpen ? '#ffffff' : '#FBC457'}
+					      className="ml-2" data-testid="dropdown__left__icon"/>
+				</Circle>
 			</div>
-			<Icon width={15} height={15} data-testid="dropdown__right_icon"
-			      name="ArrowDown"
-			      className={cn("transition-transform", "mx-5", {
-				      "transform rotate-180":
-					      isOpen,
-			      })}
-			/>
-		</button>
-	);
-};
+			{label}
+		</div>
+		<Icon width={15} height={15} data-testid="dropdown__right_icon"
+		      name="ArrowDown"
+		      className={cn("transition-transform", "mx-5", {
+			      "transform rotate-180":
+				      isOpen,
+		      })}
+		/>
+	</button>
+);
 
 DropdownButton.defaultProps = {
 	isOpen: false,
